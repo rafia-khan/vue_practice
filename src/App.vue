a1ac74177f2289c7553bda64cbec3264a4faefae
rafia-khan <khanrafia9218@gmail.com>
2024-09-16 21:00:25 +0600

    vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import HomePage from './components/HomePage.vue';
+
+  export default {
+    name:'App',
+    components:{
+      HomePage
+    }
+  }
+</script>
+
+<template>
+  <HomePage/>
+</template>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>This is Home Page</h1>
+</template>
+
+<script>
+        export default {
+            name:'HomePage',
+        }
+
+</script>
